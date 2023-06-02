e05b5e694182c666b78681dd6c0c6834b139010a
nfluk <nickylukic@gmail.com>
2023-06-02 12:18:29 +0200

    first commit

---
+++ feedLogin.js
@@ -0,0 +1,29 @@
+let database = [
+  {
+    username: 'Jim',
+    password: 'secret',
+  },
+  {
+    username: 'Alice',
+    password: 'Secret',
+  },
+  {
+    username: 'Ruben',
+    password: 'secreT',
+  },
+];
+
+let newsfeed = [
+  {
+    username: 'Bobby',
+    timeline: 'Life is great!',
+  },
+  {
+    username: 'Timmy',
+    timeline: 'I love sunny days!',
+  },
+  {
+    username: 'Sally',
+    timeline: 'My dog is great!',
+  },
+];
